9a7a6c5c60b43f07c6c7091f309f47b81564f919
Paige Sun <paige.sun.dev@gmail.com>
2019-03-14 16:24:49 -0400

    Update songs

--- songs.js
+++ songs.js
@@ -3,49 +3,62 @@ module.exports = [{
         name: "Too Deep For the Intro",
         artist: "J. Cole",
         url: "https://s3.amazonaws.com/i-make-radio-hackathon/02+-+Too+Deep+For+The+Intro.mp3",
-        length: 225 //,
-            // startTime: 0,
-            // timeElapsed: 0,
-            // isPlaying: false
+        length: 225
     },
     {
         id: 2,
         name: "38 Planes",
         artist: "Joe Smitty",
         url: "https://s3.amazonaws.com/i-make-radio-hackathon/02+-+38+Planes.mp3",
-        length: 69 //,
-            // startTime: 0,
-            // timeElapsed: 0,
-            // isPlaying: false
+        length: 69
     },
     {
         id: 3,
         name: "Once Upon a December",
         artist: "Anastasia the Musical",
         url: "https://s3.amazonaws.com/i-make-radio-hackathon/07+Once+Upon+a+December.m4a",
-        length: 179 //,
-            // startTime: 0,
-            // timeElapsed: 0,
-            // isPlaying: false
+        length: 179
     },
     {
         id: 4,
         name: "Let It Go",
         artist: "Frozen",
-        url: "https://s3.amazonaws.com/i-make-radio-hackathon/21+Finale+_+Let+It+Go+(From+_Frozen_+The+Broadway+Musical_).mp3",
-        length: 151 //,
-            // startTime: 0,
-            // timeElapsed: 0,
-            // isPlaying: false
+        url: "https://s3.amazonaws.com/i-make-radio-hackathon/13+Let+It+Go+(From+_Frozen_+The+Broadway+Musical_).mp3",
+        length: 151
     },
     {
         id: 5,
         name: "Eastside",
         artist: "Halsey",
         url: "https://s3.amazonaws.com/i-make-radio-hackathon/Halsey+-+Eastside+SNL+perf.mp3",
-        length: 210 //,
-            // startTime: 0,
-            // timeElapsed: 0,
-            // isPlaying: false
+        length: 210
+    },
+    {
+        id: 6,
+        name: "Journey to the Past",
+        artist: "Anastasia the Musical",
+        url: "https://s3.amazonaws.com/i-make-radio-hackathon/11+Journey+to+the+Past.m4a",
+        length: 199
+    },
+    {
+        id: 7,
+        name: "A Million Dreams Instrumental",
+        artist: "The Greatest Showman",
+        url: "https://s3.amazonaws.com/i-make-radio-hackathon/a_million_dreams_piano_instrumental_-_the_greatest_showman.mp3",
+        length: 298
+    },
+    {
+        id: 8,
+        name: "Screech In",
+        artist: "Come From Away",
+        url: "https://s3.amazonaws.com/i-make-radio-hackathon/15+-+Screech+In.mp3",
+        length: 244
+    },
+    {
+        id: 8,
+        name: "Promise the Star Instrumental",
+        artist: "BiSH",
+        url: "https://s3.amazonaws.com/i-make-radio-hackathon/BiSH+Off+Vocal+-+Promise+the+Star%E3%80%90%E8%80%B3%E3%82%B3%E3%83%92%E3%82%9A%E3%80%91%E3%83%95%E3%82%9A%E3%83%AD%E3%83%9F%E3%82%B9%E3%82%B5%E3%82%99%E3%82%B9%E3%82%BF%E3%83%BC+-+BiSH+-.mp3",
+        length: 275
     }
 ]
