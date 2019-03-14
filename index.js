a43cdec9c6d6c196fb101a8cbbe3ba2c69820bc5
Paige Sun <psun@tribalscale.com>
2019-03-14 01:40:56 -0400

    Unsecurity

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ io.on('connection', (socket) => {
   })
 });
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+ })
+
 app.get('/allSongs', (req, res) => {
   res.send(songs)
 })
